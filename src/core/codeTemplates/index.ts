43e079428374c8bbd68da55ee31ea271dd74da78
Jitendra Tiwari <jkjitendra725@gmail.com>
2026-01-21 21:11:13 +0530

    feat(core): Register all new algorithm templates in registry
     - Import and register newly added algorithm templates across all categories
     - Integrate templates for Strings, Stacks, Queues, Linked Lists, Recursion, Trees, Heaps, Hashing, Graphs, DP, and Greedy algorithms
     - Update codeTemplatesRegistry to ensure all 100+ new algorithms are available in the application

--- src/core/codeTemplates/index.ts
+++ src/core/codeTemplates/index.ts
@@ -48,6 +48,142 @@ import { mergeSortedArraysCode } from './mergeSortedArrays';
 import { rotateArrayCode } from './rotateArray';
 import { arrayRearrangementCode } from './arrayRearrangement';
 import { nextPermutationCode } from './nextPermutation';
+// Strings algorithms
+import { stringOperationsCode } from './stringOperations';
+import { characterFrequencyCode } from './characterFrequency';
+import { bruteForceSearchCode } from './bruteForceSearch';
+import { kmpCode } from './kmp';
+import { rabinKarpCode } from './rabinKarp';
+import { zAlgorithmCode } from './zAlgorithm';
+import { boyerMooreCode } from './boyerMoore';
+import { anagramDetectionCode } from './anagramDetection';
+import { longestPalindromicSubstringCode } from './longestPalindromicSubstring';
+import { longestCommonSubstringCode } from './longestCommonSubstring';
+import { stringRotationCode } from './stringRotation';
+import { removeDuplicatesCode } from './removeDuplicates';
+// Stacks algorithms
+import { stackOperationsCode } from './stackOperations';
+import { balancedParenthesesCode } from './balancedParentheses';
+import { infixToPostfixCode } from './infixToPostfix';
+import { infixToPrefixCode } from './infixToPrefix';
+import { postfixEvalCode } from './postfixEval';
+import { prefixEvalCode } from './prefixEval';
+import { nextGreaterElementCode } from './nextGreaterElement';
+import { nextSmallerElementCode } from './nextSmallerElement';
+import { stockSpanCode } from './stockSpan';
+import { largestRectangleHistogramCode } from './largestRectangleHistogram';
+import { validStackSequencesCode } from './validStackSequences';
+// Queues algorithms
+import { queueOperationsCode } from './queueOperations';
+import { circularQueueCode } from './circularQueue';
+import { dequeCode } from './deque';
+import { priorityQueueCode } from './priorityQueue';
+import { queueUsingStacksCode } from './queueUsingStacks';
+import { stackUsingQueuesCode } from './stackUsingQueues';
+import { lruCacheCode } from './lruCache';
+import { slidingWindowMaximumCode } from './slidingWindowMaximum';
+import { firstNonRepeatingCharCode } from './firstNonRepeatingChar';
+import { generateBinaryNumbersCode } from './generateBinaryNumbers';
+import { circularTourCode } from './circularTour';
+// Linked Lists algorithms
+import { singlyLinkedListCode } from './singlyLinkedList';
+import { doublyLinkedListCode } from './doublyLinkedList';
+import { circularLinkedListCode } from './circularLinkedList';
+import { circularDoublyLinkedListCode } from './circularDoublyLinkedList';
+import { reverseLinkedListCode } from './reverseLinkedList';
+import { findMiddleCode } from './findMiddle';
+import { mergeSortedListsCode } from './mergeSortedLists';
+import { removeNthFromEndCode } from './removeNthFromEnd';
+import { detectCycleCode } from './detectCycle';
+import { findCycleStartCode } from './findCycleStart';
+import { palindromeLinkedListCode } from './palindromeLinkedList';
+import { intersectionPointCode } from './intersectionPoint';
+import { rotateListCode } from './rotateList';
+import { flattenMultilevelCode } from './flattenMultilevel';
+// Recursion & Backtracking algorithms
+import { factorialCode } from './factorial';
+import { fibonacciRecursiveCode } from './fibonacciRecursive';
+import { sumOfDigitsCode } from './sumOfDigits';
+import { powerRecursiveCode } from './powerRecursive';
+import { gcdCode } from './gcd';
+import { towerOfHanoiCode } from './towerOfHanoi';
+import { binarySearchRecursiveCode } from './binarySearchRecursive';
+import { nQueensCode } from './nQueens';
+import { sudokuSolverCode } from './sudokuSolver';
+import { ratInMazeCode } from './ratInMaze';
+import { permutationsCode } from './permutations';
+// Trees algorithms
+import { inorderTraversalCode } from './inorderTraversal';
+import { preorderTraversalCode } from './preorderTraversal';
+import { postorderTraversalCode } from './postorderTraversal';
+import { levelOrderTraversalCode } from './levelOrderTraversal';
+import { treeHeightCode } from './treeHeight';
+import { isBalancedCode } from './isBalanced';
+import { isBSTCode } from './isBST';
+import { bstOperationsCode } from './bstOperations';
+import { lowestCommonAncestorCode } from './lowestCommonAncestor';
+// Heaps algorithms
+import { maxHeapCode } from './maxHeap';
+import { minHeapCode } from './minHeap';
+import { buildHeapCode } from './buildHeap';
+import { kthLargestCode } from './kthLargest';
+import { kthSmallestCode } from './kthSmallest';
+import { mergeKSortedListsCode } from './mergeKSortedLists';
+import { topKFrequentCode } from './topKFrequent';
+import { medianOfStreamCode } from './medianOfStream';
+// Hashing algorithms
+import { hashFunctionsCode } from './hashFunctions';
+import { chainingCode } from './chaining';
+import { openAddressingCode } from './openAddressing';
+import { rehashingCode } from './rehashing';
+import { twoSumHashCode } from './twoSumHash';
+import { groupAnagramsCode } from './groupAnagrams';
+import { longestConsecutiveCode } from './longestConsecutive';
+import { subarrayZeroSumCode } from './subarrayZeroSum';
+import { countDistinctWindowCode } from './countDistinctWindow';
+// Graphs algorithms
+import { adjacencyMatrixCode } from './adjacencyMatrix';
+import { adjacencyListCode } from './adjacencyList';
+import { bfsCode } from './bfs';
+import { dfsCode } from './dfs';
+import { dijkstraCode } from './dijkstra';
+import { bellmanFordCode } from './bellmanFord';
+import { floydWarshallCode } from './floydWarshall';
+import { aStarCode } from './aStar';
+import { kruskalMSTCode } from './kruskalMST';
+import { primMSTCode } from './primMST';
+import { kahnTopologicalSortCode } from './kahnTopologicalSort';
+import { dfsTopologicalSortCode } from './dfsTopologicalSort';
+import { connectedComponentsCode } from './connectedComponents';
+import { kosarajuSCCCode } from './kosarajuSCC';
+import { tarjanSCCCode } from './tarjanSCC';
+import { cycleDetectionCode } from './cycleDetectionGraph';
+// Dynamic Programming algorithms
+import { fibonacciDPCode } from './fibonacciDP';
+import { climbingStairsCode } from './climbingStairs';
+import { houseRobberCode } from './houseRobber';
+import { lisCode } from './lis';
+import { coinChangeCode } from './coinChange';
+import { knapsack01Code } from './knapsack01';
+import { lcsCode } from './lcs';
+import { editDistanceCode } from './editDistance';
+import { matrixChainMultiplicationCode } from './matrixChain';
+import { uniquePathsCode } from './uniquePaths';
+import { minPathSumCode } from './minPathSum';
+import { subsetSumCode } from './subsetSum';
+import { partitionEqualSubsetCode } from './partitionEqualSubset';
+import { rodCuttingCode } from './rodCutting';
+import { wordBreakCode } from './wordBreak';
+// Greedy algorithms
+import { activitySelectionCode } from './activitySelection';
+import { jobSequencingCode } from './jobSequencing';
+import { meetingRoomsCode } from './meetingRooms';
+import { fractionalKnapsackCode } from './fractionalKnapsack';
+import { huffmanCodingCode } from './huffmanCoding';
+import { minimumPlatformsCode } from './minimumPlatforms';
+import { mergeIntervalsCode } from './mergeIntervals';
+import { insertIntervalCode } from './insertInterval';
+import { nonOverlappingIntervalsCode } from './nonOverlappingIntervals';
 
 // Registry of all code templates
 const codeTemplatesRegistry: Map<string, AlgorithmCodeTemplates> = new Map();
@@ -99,6 +235,142 @@ const codeTemplatesRegistry: Map<string, AlgorithmCodeTemplates> = new Map();
   rotateArrayCode,
   arrayRearrangementCode,
   nextPermutationCode,
+  // Strings
+  stringOperationsCode,
+  characterFrequencyCode,
+  bruteForceSearchCode,
+  kmpCode,
+  rabinKarpCode,
+  zAlgorithmCode,
+  boyerMooreCode,
+  anagramDetectionCode,
+  longestPalindromicSubstringCode,
+  longestCommonSubstringCode,
+  stringRotationCode,
+  removeDuplicatesCode,
+  // Stacks
+  stackOperationsCode,
+  balancedParenthesesCode,
+  infixToPostfixCode,
+  infixToPrefixCode,
+  postfixEvalCode,
+  prefixEvalCode,
+  nextGreaterElementCode,
+  nextSmallerElementCode,
+  stockSpanCode,
+  largestRectangleHistogramCode,
+  validStackSequencesCode,
+  // Queues
+  queueOperationsCode,
+  circularQueueCode,
+  dequeCode,
+  priorityQueueCode,
+  queueUsingStacksCode,
+  stackUsingQueuesCode,
+  lruCacheCode,
+  slidingWindowMaximumCode,
+  firstNonRepeatingCharCode,
+  generateBinaryNumbersCode,
+  circularTourCode,
+  // Linked Lists
+  singlyLinkedListCode,
+  doublyLinkedListCode,
+  circularLinkedListCode,
+  circularDoublyLinkedListCode,
+  reverseLinkedListCode,
+  findMiddleCode,
+  mergeSortedListsCode,
+  removeNthFromEndCode,
+  detectCycleCode,
+  findCycleStartCode,
+  palindromeLinkedListCode,
+  intersectionPointCode,
+  rotateListCode,
+  flattenMultilevelCode,
+  // Recursion & Backtracking
+  factorialCode,
+  fibonacciRecursiveCode,
+  sumOfDigitsCode,
+  powerRecursiveCode,
+  gcdCode,
+  towerOfHanoiCode,
+  binarySearchRecursiveCode,
+  nQueensCode,
+  sudokuSolverCode,
+  ratInMazeCode,
+  permutationsCode,
+  // Trees
+  inorderTraversalCode,
+  preorderTraversalCode,
+  postorderTraversalCode,
+  levelOrderTraversalCode,
+  treeHeightCode,
+  isBalancedCode,
+  isBSTCode,
+  bstOperationsCode,
+  lowestCommonAncestorCode,
+  // Heaps
+  maxHeapCode,
+  minHeapCode,
+  buildHeapCode,
+  kthLargestCode,
+  kthSmallestCode,
+  mergeKSortedListsCode,
+  topKFrequentCode,
+  medianOfStreamCode,
+  // Hashing
+  hashFunctionsCode,
+  chainingCode,
+  openAddressingCode,
+  rehashingCode,
+  twoSumHashCode,
+  groupAnagramsCode,
+  longestConsecutiveCode,
+  subarrayZeroSumCode,
+  countDistinctWindowCode,
+  // Graphs
+  adjacencyMatrixCode,
+  adjacencyListCode,
+  bfsCode,
+  dfsCode,
+  dijkstraCode,
+  bellmanFordCode,
+  floydWarshallCode,
+  aStarCode,
+  kruskalMSTCode,
+  primMSTCode,
+  kahnTopologicalSortCode,
+  dfsTopologicalSortCode,
+  connectedComponentsCode,
+  kosarajuSCCCode,
+  tarjanSCCCode,
+  cycleDetectionCode,
+  // Dynamic Programming
+  fibonacciDPCode,
+  climbingStairsCode,
+  houseRobberCode,
+  lisCode,
+  coinChangeCode,
+  knapsack01Code,
+  lcsCode,
+  editDistanceCode,
+  matrixChainMultiplicationCode,
+  uniquePathsCode,
+  minPathSumCode,
+  subsetSumCode,
+  partitionEqualSubsetCode,
+  rodCuttingCode,
+  wordBreakCode,
+  // Greedy Algorithms
+  activitySelectionCode,
+  jobSequencingCode,
+  meetingRoomsCode,
+  fractionalKnapsackCode,
+  huffmanCodingCode,
+  minimumPlatformsCode,
+  mergeIntervalsCode,
+  insertIntervalCode,
+  nonOverlappingIntervalsCode,
 ].forEach((template) => {
   codeTemplatesRegistry.set(template.algorithmId, template);
 });
