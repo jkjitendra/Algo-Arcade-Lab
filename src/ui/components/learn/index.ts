dd48105660ee4c2d75ae60f9478bc0d51ca098eb
Jitendra Tiwari <jkjitendra725@gmail.com>
2026-01-22 23:52:56 +0530

    refactor(learn): export all learn components
     - Created a barrel file to export all new MDX-compatible components.
     - Simplifies imports in MDX files and other parts of the application.
     - Includes exports for VisualizeLink, StepByStep, CodeTabs, ComplexityTable, and others.

---
+++ src/ui/components/learn/index.ts
@@ -0,0 +1,10 @@
+// Learn page components barrel export
+export { LearnCard } from "./LearnCard";
+export { ComplexityTable } from "./ComplexityTable";
+export { ExampleBox } from "./ExampleBox";
+export { CodeTabs } from "./CodeTabs";
+export { Callout } from "./Callout";
+export { Analogy } from "./Analogy";
+export { StepByStep } from "./StepByStep";
+export { StaticDiagram, ArrayDiagram } from "./StaticDiagram";
+export { VisualizeLink } from "./VisualizeLink";
